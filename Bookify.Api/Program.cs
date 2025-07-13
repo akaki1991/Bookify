a0e603033c3f6cba01b6eb3d83c2e5565ed30538
akaki <akakichkopoia@gmail.com>
2025-07-13 14:47:00 +0400

    CI changes

--- Bookify.Api/Program.cs
+++ Bookify.Api/Program.cs
@@ -24,7 +24,6 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
 
-
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
